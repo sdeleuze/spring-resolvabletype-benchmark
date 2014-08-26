705a84c33f1cb3a834caf72d0fbb54022be61b64
Sebastien Deleuze <sdeleuze@pivotal.io>
2014-08-26 11:54:41 +0200

    Add Spring 4.1.0.RC2 benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 mvn clean package -Dspring.version=3.1.4.RELEASE -Dspring.flex.version=1.5.2.RELEASE && java -jar target/microbenchmarks.jar -wi 5 -i 5 -f 1
+mvn clean package -Dspring.version=4.1.0.RC2 -Dspring.flex.version=1.6.0.BUILD-SNAPSHOT && java -jar target/microbenchmarks.jar -wi 5 -i 5 -f 1
 mvn clean package -Dspring.version=4.1.0.BUILD-SNAPSHOT -Dspring.flex.version=1.6.0.BUILD-SNAPSHOT && java -jar target/microbenchmarks.jar -wi 5 -i 5 -f 1
